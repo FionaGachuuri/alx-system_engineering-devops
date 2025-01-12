f6f5e743fee0dfd3d836fd9dd45ce5953265a305
FionaGachuuri <fionairuka@gmail.com>
2025-01-12 11:05:12 +0300

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,14 @@
+# This puppet manifest installs flask version 2.1.0 using pip3
+
+# ensuring pip3 is installed
+package { 'python3-pip':
+	ensure => installed,
+}
+
+# install flask version 2.1.0 using pip3 and werkzeug version 2.1.1
+exec {'install_flask_and_werkzeug':
+	command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==3.1.3',
+	path => [ '/usr/bin', 'usr/local/bin'],
+	unless => '/usr/bin/pip3 show flask | grep Version | grep 2.1.0 && /usr/bin/pip3 show werkzeug | grep Version | grep 3.1.3',
+	environment => ['Home=/root', 'PATH=/usr/bin:/usr/local/bin'],
+}
