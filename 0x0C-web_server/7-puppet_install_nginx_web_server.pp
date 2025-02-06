7a59ab69b00bba16b99b4623f2f25a8c91e72cf2
FionaGachuuri <fionairuka@gmail.com>
2025-02-06 12:07:52 +0300

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,36 @@
-# automating Nginx configuration using puppet
+# Puppet script to install and configure Nginx with a redirect and custom index page
 
+# Install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
+# Ensure Nginx is running and enabled on boot
 service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
 }
 
-file_line { 'add_redirect_rule':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  line   => 'rewrite ^/redirect_me https://www.github.com/FionaGachuuri permanent;',
-  match  => '^listen 80 default_server;',
+# Modify the default Nginx configuration to add a redirect
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensure the symlink exists in sites-enabled
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
   notify => Service['nginx'],
 }
 
+# Create a custom index.html file
 file { '/var/www/html/index.html':
+  ensure  => file,
   content => 'Hello World!',
-}
-
-service { 'nginx':
-  ensure  => running,
   require => Package['nginx'],
 }
