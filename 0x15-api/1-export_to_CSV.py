44106c750d23c93e8aac2c00e66389476f6fe1fb
FionaGachuuri <fionairuka@gmail.com>
2025-02-12 10:04:35 +0300

    . Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module retrieves and displays an employee's
+TODO list progress using a REST API
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
