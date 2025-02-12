a3ebf7dae3aeafd0e7d3c5c2109258e1ea151d70
FionaGachuuri <fionairuka@gmail.com>
2025-02-12 09:51:34 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,44 +3,32 @@
 This module retrieves and displays an employee's
 TODO list progress using a REST API
 """
+
 import requests
 import sys
 
 
-def fetch_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com/"
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    # Fetch user information
-    user_url = f"{base_url}users/{employee_id}"
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("Error: User not found")
-        return
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    # Fetch user's TODO list
-    todos_url = f"{base_url}todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print("Error: Unable to fetch TODO list")
-        return
-    todos_data = todos_response.json()
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    # Calculate completed tasks
-    completed_tasks = [task for task in todos_data if task.get("completed")]
-    total_tasks = len(todos_data)
-    done_tasks = len(completed_tasks)
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    # Display results
-    print(f"Employee {employee_name} is done with tasks
-          ({done_tasks}/{total_tasks}): ")
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    else:
-        fetch_todo_progress(int(sys.argv[1]))
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
