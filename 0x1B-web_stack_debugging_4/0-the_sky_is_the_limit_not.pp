6f288232f46c6e4e91d9b656db73fc79462a99a1
FionaGachuuri <fionairuka@gmail.com>
2025-03-06 09:43:53 +0300

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# This manifest updates the Nginx configuration to increase the open limit from 15 to 4096 and then restarts Nginx.
 exec { 'nginx fix':
   onlyif   => 'test -e /etc/default/nginx',
   command  => "sed -i 's/-n 15/-n 4096/g' /etc/default/nginx",
